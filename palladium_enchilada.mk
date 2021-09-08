fcbe7572693f4f2d07dd581ef79818e68d56f41b
author zahidmgsk <zahidmgsk@gmail.com>
2021-09-08 06:36:40 +0000

    enchilada: Switch to coral Aug FP
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- palladium_enchilada.mk
+++ palladium_enchilada.mk
@@ -61,6 +61,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus6 \
     PRODUCT_NAME=OnePlus6 \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ3A.210805.001.A1 7474174 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RQ3A.210805.001.A1/7474174:user/release-keys
